9fffae63752e0672515d43c0256f7197148ddc3c
Kamal Kamalaldin <kamal@Kamals-MBP.localdomain>
2018-08-09 13:51:13 -0400

    added tmux configuration

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
-sudo apt-get install vim
+# basic package installaion
+sudo apt-get install vim curl git zsh
 
+#setup zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# alias, vim, and tmux setup
 cp aliases ~/.aliases.sh
-cp shell_helpers ~/.shell_helpers
-cat zshrc >> ~/.zshrc
+cp shell_helpers ~/.shell_helpers.sh
+cp zshrc  ~/.zshrc
 cp vimrc ~/.vimrc
 vim +PlugInstall +qall
