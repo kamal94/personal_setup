ed368e1232164fe597ae6f91309d5c5b0f2929a5
Kamal Kamalaldin <kamal@Kamals-MBP.localdomain>
2018-08-09 14:01:41 -0400

    made zsh optional

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
+# zsh is default installation but you can specify bash too
+SHELL_OPTION=${1:-zsh} 
 # basic package installaion
-sudo apt-get install vim curl git zsh
+sudo apt-get install vim curl git 
 
 #setup zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ "$SHELL_OPTIONl" = "zsh" ]; then
+  sudo apt-get install zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  cp zshrc  ~/.zshrc
+fi
 
 # alias, vim, and tmux setup
 cp aliases ~/.aliases.sh
 cp shell_helpers ~/.shell_helpers.sh
-cp zshrc  ~/.zshrc
 cp vimrc ~/.vimrc
 vim +PlugInstall +qall
