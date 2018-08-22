bf7ecaed3676ba9479f2bd406238a04ac5e355af
kamal <kamal@clinc.com>
2018-08-22 15:54:44 -0400

    Added pip3 and fixed zsh intallation

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 # zsh is default installation but you can specify bash too
 SHELL_OPTION=${1:-zsh} 
 # basic package installaion
-sudo apt-get install vim curl git ack
+sudo apt-get install vim curl git ack-grep tmux python3-pip
 
 #setup zsh
-if [ "$SHELL_OPTIONl" = "zsh" ]; then
+echo "shell option:"
+echo $SHELL_OPTION
+echo "$SHELL_OPTION"
+if [ "$SHELL_OPTION" == "zsh" ]; then
+  echo "shell option:"
+  echo $SHELL_OPTION
   sudo apt-get install zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   cp zshrc  ~/.zshrc
@@ -13,7 +18,9 @@ fi
 # alias, vim, and tmux setup
 pip3 install pylint
 cp aliases ~/.aliases.sh
+source ~/.aliases.sh
 cp shell_helpers ~/.shell_helpers.sh
+source ~/.shell_helpers.sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vimrc ~/.vimrc
 vim +PluginInstall +qall
--- mac_install.sh
+++ mac_install.sh
@@ -1,10 +1,10 @@
 # zsh is default installation but you can specify bash too
 SHELL_OPTION=${1:-zsh} 
 # basic package installaion
-brew install vim curl git cmake ack
+brew install vim curl git cmake ack tmux
 
 #setup zsh
-if [ "$SHELL_OPTIONl" = "zsh" ]; then
+if [ "$SHELL_OPTION" == "zsh" ]; then
   echo "installign zsh"
   brew install zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
